e69e3351492c9ac6f959b2083d9ed2a737fdec28
ImWG <zhwuji233>
2020-03-05 16:06:33 +0800

    Beta 1.3 to 1.4.

--- src/com/imwg/smxworkshop/model/Configuration.java
+++ src/com/imwg/smxworkshop/model/Configuration.java
@@ -11,7 +11,7 @@ import com.imwg.smxworkshop.view.ViewConfig;
 final public class Configuration {
 	public static final String configName = "config.properties";
 	public static final String gitHubPath = "https://github.com/ImWG/SMX-Workshop.git";
-	public static final String VERSION = "1.3 Beta";
+	public static final String VERSION = "1.4 Beta";
 	
 	static private Properties properties;
 	static private int languageId = 1033;
